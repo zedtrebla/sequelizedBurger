f089d28af2c6e7cd4b0188dc7ffafb483623ec08
Bert <zedtrebla@aol.com>
2017-05-07 00:05:05 -0500

    final

---
+++ controllers/burgers_Controller.js
@@ -0,0 +1,50 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  cat.create([
+    "burger_name", "devoured", "ts"
+  ], [
+    req.body.burger_name, req.body.devoured, req.ts
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name, devoured, ts) VALUES ('cheese burger', true, CURRENT_TIMESTAMP);
+INSERT INTO burger (name, devoured, ts) VALUES ('burger burger', true, CURRENT_TIMESTAMP);
+INSERT INTO burger (name, devoured, ts) VALUES ('jalapeno burger', true, CURRENT_TIMESTAMP);
